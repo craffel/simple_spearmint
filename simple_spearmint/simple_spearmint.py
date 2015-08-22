ad06873b30de4bb372e2d8cd40b632bfd1ae2d76
Colin Raffel <craffel@gmail.com>
2015-08-22 17:34:56 -0400

    Add comments

--- simple_spearmint/simple_spearmint.py
+++ simple_spearmint/simple_spearmint.py
@@ -44,17 +44,25 @@ class SimpleSpearmint(object):
     """
 
     def __init__(self, parameter_space, noiseless=False, debug=False):
+        # Add the 'size' key to each entry in the parameter space.
+        # We assume all parameters are size 1, which is reasonable.
         for name, spec in parameter_space.items():
             spec['size'] = 1
             parameter_space[name] = spec
+        # Convert the "noiseless" bool flag to Spearmint's string semantics
         noiseless = 'NOISELESS' if noiseless else 'GAUSSIAN'
+        # Set up task configuration dict
         self.task_config = {'main': {'type': 'objective',
                                      'likelihood': noiseless}}
+        # Create a "task group" for this experiment
         self.task_group = spearmint.tasks.task_group.TaskGroup(
             self.task_config, parameter_space)
+        # Use Spearmint's default chooser
         self.chooser = spearmint.choosers.default_chooser.init({})
+        # Initialize lists of parameter and objective value trials
         self.parameter_values = []
         self.objective_values = []
+        # We need to persistently store the model hyperparameters
         self.hypers = None
         self.debug = debug
 
@@ -77,11 +85,15 @@ class SimpleSpearmint(object):
         """
         specd_parameter_values = {}
         for name, value in parameter_values.items():
+            # Retrieve the param type string from the variable spec
             param_type = self.task_group.variables_config[name]['type']
+            # If the variable type is an enum, make the value a list
             if param_type == 'enum':
                 values = [value]
             else:
                 values = value
+            # Create an entry in the "specd" parameter values which will make
+            # spearmint happy
             specd_parameter_values[name] = {'type': param_type,
                                             'values': values}
         return specd_parameter_values
@@ -99,11 +111,14 @@ class SimpleSpearmint(object):
             parameters.
 
         """
+        # Add this parameter setting and objective value to our list of trials
         self.parameter_values.append(parameter_values)
         self.objective_values.append(objective_value)
+        # Update the task group with these parameter settings
         self.task_group.inputs = np.array(
             [self.task_group.vectorify(self.spec_parameter_values(values))
              for values in self.parameter_values])
+        # Update the task group with the objective value
         self.task_group.values = {'main': np.array(self.objective_values)}
 
     def suggest(self):
@@ -114,16 +129,22 @@ class SimpleSpearmint(object):
         suggestion : dict
             Dictionary mapping parameter names to the suggested values.
         """
+        # When not debugging, redirect sys.stderr to devnull!
         if not self.debug:
             old_stderr = sys.stderr
             sys.stderr = open(os.devnull, 'w')
+        # Update the model hyperparameters given the current trial list
         self.hypers = self.chooser.fit(
             self.task_group, self.hypers, self.task_config)
+        # Get a parameter suggestion
         suggestion = self.chooser.suggest()
         if not self.debug:
             sys.stderr.close()
             sys.stderr = old_stderr
+        # Convert the vector format returned by chooser.suggest() to a dict
         suggestion = self.task_group.paramify(np.atleast_1d(suggestion))
+        # Retrieve the values, and also flatten the 1d arrays that spearmint
+        # forces you to use
         suggestion = dict((name, value['values'][0])
                           for name, value in suggestion.items())
         return suggestion
@@ -140,6 +161,7 @@ class SimpleSpearmint(object):
         objective_value : float
             The lowest objective function value achieved.
         """
+        # Retrieve the index of the lowest objective value
         best_objective = np.argmin(self.objective_values)
         return (self.parameter_values[best_objective],
                 self.objective_values[best_objective])
